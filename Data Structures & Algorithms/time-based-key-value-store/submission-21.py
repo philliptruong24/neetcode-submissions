af0531d7b3f987be54eeb6364178a0b3fe8445f2
Phillip Truong <179678319+philliptruong24@users.noreply.github.com>
2026-08-19 22:19:06 -0700

    Add: time-based-key-value-store - submission-21

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-21.py
@@ -0,0 +1,23 @@
+class TimeMap:
+
+    def __init__(self):
+        self.hashmap = defaultdict(list)
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.hashmap[key].append((timestamp, value))
+
+    def get(self, key: str, timestamp: int) -> str:
+        if key not in self.hashmap:
+            return ""
+        l = 0
+        r = len(self.hashmap[key]) - 1
+
+        while l < r:
+            mid = l + (r-l +1) // 2
+            
+            if self.hashmap[key][mid][0] <= timestamp:
+                l = mid
+            else:
+                r = mid -1
+        
+        return self.hashmap[key][l][1] if self.hashmap[key][l][0] <= timestamp else ""
