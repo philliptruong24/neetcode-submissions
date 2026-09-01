6c8e8a208a2b1e0dd3c0284d3afe68fb30dae00a
Phillip Truong <179678319+philliptruong24@users.noreply.github.com>
2026-08-31 17:55:27 -0700

    Add: design-twitter-feed - submission-5

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-5.py
@@ -0,0 +1,46 @@
+class Twitter:
+
+    def __init__(self):
+        self.hashtable = {}
+        self.recency = 0
+
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        if userId not in self.hashtable:
+            self.hashtable[userId] = ([], [])
+
+        self.hashtable[userId][1].append((self.recency, tweetId))
+        self.recency += 1
+
+    def getNewsFeed(self, userId: int) -> List[int]:
+        minHeap = []
+        arr = [userId]
+        for followingId in self.hashtable[userId][0]:
+            arr.append(followingId)
+
+        for personId in arr:
+            for recency, tweetId in self.hashtable[personId][1]:
+                heapq.heappush(minHeap, (-recency, tweetId))
+        
+        res = []
+        for i in range(10):
+            if minHeap:
+                recency, tweetId = heapq.heappop(minHeap)
+                res.append(tweetId)
+        
+        return res
+
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        if followerId not in self.hashtable:
+            self.hashtable[followerId] = ([], [])
+        
+        if followeeId not in self.hashtable[followerId][0]:    
+            self.hashtable[followerId][0].append(followeeId)
+        
+
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        if followerId in self.hashtable and followeeId in self.hashtable[followerId][0]:
+
+            self.hashtable[followerId][0].remove(followeeId)
+        
