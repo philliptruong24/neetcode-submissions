59259f8a15caed6609e7ee56a0eea899045ad4bf
Phillip Truong <179678319+philliptruong24@users.noreply.github.com>
2026-09-04 18:35:21 -0700

    Add: subsets-ii - submission-2

---
+++ Data Structures & Algorithms/subsets-ii/submission-2.py
@@ -0,0 +1,25 @@
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        res = []
+        curr = []
+        
+        def dfs(i):
+            res.append(curr.copy())
+            if i == len(nums):
+                return
+            
+            for j in range(i, len(nums)):
+                if j > i and nums[j] == nums[j - 1]:
+                    continue
+
+                curr.append(nums[j])
+                dfs(j + 1)
+
+                curr.pop()
+
+        dfs(0)
+        return res
+        
+
+        
